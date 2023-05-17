3c1e797bdee7de322a6a2d3ab6b9350ac7327d98
hb02 <sascha@richter.tirol>
2023-05-17 16:50:29 +0200

    update

--- basicinstall.sh
+++ basicinstall.sh
@@ -14,3 +14,5 @@ sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-li
 sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb -n
 #Download jetbrainsToolbox
 sudo wget "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.28.1.15219.tar.gz"
+#Installing Discord
+sudo snap install discord
